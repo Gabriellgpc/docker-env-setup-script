04f7a45f31d6939f2445ae4a8d8580c8b26623a7
Condados <condadoslgpc@gmail.com>
2022-06-11 19:53:30 -0400

    docker with gpu

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# docker-env-setup-script
+# Install Docker and Docker Compose
 
 Apenas rode o script e confirme (y) o que pedir e o docker e o docker-compose serão instalados corretamente na maquina.
+
+
+# To use GPU with Docker
+- If you do not have the driver of  the GPU installed. Try to use `$nvidia-smi`, if works you already have the driver installed, if not ([a good tutorial](https://linuxconfig.org/how-to-install-the-nvidia-drivers-on-ubuntu-20-04-focal-fossa-linux)):
+    `sudo ubuntu-drivers autoinstall`
+
+- Follow this instruction to install the cuda toolkit: https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html#docker
--- setup-docker-env.sh
+++ setup-docker-env.sh
@@ -46,3 +46,22 @@ docker run hello-world
 # docker-compose
 
 sudo apt install docker-compose
+
+echo "DOCKER AND DOCKER COMPOSE INSTALLED SUCCESSFULY"
+
+echo "STEP 2: INSTALL CUDA TOOL KIT AND DOCKER COMPATIBILITY"
+
+# Setting up NVIDIA Container Toolkit¶
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+sudo apt-get update
+
+sudo apt-get install -y nvidia-docker2
+
+sudo systemctl restart docker
+
+docker run --rm --gpus all nvidia/cuda:11.0.3-base-ubuntu20.04 nvidia-smi
